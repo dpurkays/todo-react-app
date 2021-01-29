299dafe7864b21d56714d7b208ba59f76bd55fb3
Dula Purkaystha <dpurkays@gmail.com>
2021-01-28 18:26:49 -0800

    Todo list has complete and trash button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
-const Form = ({setInputText}) => {
-    const inputTextHandler = (e) => {
-        console.log(e.target.value);
-        setInputText(e.target.value);
-    };
+const Form = ({setInputText, todos, setTodos, inputText}) => {
+
+   const inputTextHandler = (e) => {
+       console.log(e.target.value);
+       setInputText(e.target.value);
+   };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+        ...todos,
+        {text: inputText, completed: false, id: Math.random() * 1000}
+    ])
+  };
+
+  //set the state back to blank
+  setInputText("");
 
     return(
 
     //code copied from https://github.com/developedbyed/vanilla-todo/blob/master/index.html
      <form>
-          <input onChange={inputTextHandler} type="text" className="todo-input" />
-          <button className="todo-button" type="submit">
+          <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+          <button onClick={submitTodoHandler} className="todo-button" type="submit">
             <i className="fas fa-plus-square"></i>
           </button>
           <div className="select">
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,17 @@
 import React from 'react';
+//import components
+import Todo from './Todo';
 
 const TodoList = () => {
     return (
     <div className="todo-container">
-        <ul className="todo-list"></ul>
+        <ul className="todo-list">
+        <Todo />
+        </ul>
     </div>
     );
 }
+
+
 
 export default TodoList;
